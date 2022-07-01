bc25996e1aabff63cf409b71512f04bb4d6df2d5
Jampolo <jp.1380@gmail.com>
2022-07-01 06:58:29 -0400

    progreso en posteo de noticias

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Header from './componentes/header';
 import Login from './componentes/login';
 import Footer from './componentes/footer';
+import NewNoticia from './componentes/newNoticia';
+import Noticias from './componentes/noticias';
 
 function App() {
   return (
     <div>
       <Header/>
       <div className='body'>
-
+        <NewNoticia/>
+        <Noticias/>
       </div>
       <Footer/>
     </div>
